190ac5e102d62877f19c2851fc4ace04a7524b17
Utilisateur <user@debian.arcahe.ovh>
2019-01-29 12:28:48 +0100

    td1 405

---
+++ TD1 IN 405/fctsyst.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+int main()
+{
+	//system("cd /tmp; ls -R projectV2");
+	//pour exo 4
+	
+	system("cd /tmp; mkdir projet; cd projet/; mkdir doc; mkdir include; mkdir src; echo PITTIS THOMAS README.txt; cd include/; touch func.h; cd ..;cd src/; touch main.c; touch func.c; cd ..; cd ..; ls -R >contents.txt; cp -r projet projetV2; rm -fr projet; tar -cf p2.tar projetV2");
+	//exo 6
+	
+}
